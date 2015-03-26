0eb73646b8027cc6b91bf30a47b2cdf00ae877c9
Nick Weschkalnies <info@medianetic.de>
2015-03-26 09:36:16 +0100

    Removed comments

--- system/modules/newsreaderpagination/NewsreaderPagination.php
+++ system/modules/newsreaderpagination/NewsreaderPagination.php
@@ -5,8 +5,6 @@
 			global $objPage;
 
 			$result = \Database::getInstance()->prepare("SELECT id,alias FROM tl_news WHERE pid=? AND published=? ORDER BY time DESC, date DESC")->execute($pid, 1);  
-			//SELECT * FROM `tl_news` WHERE pid=2 ORDER BY `tl_news`.`date` DESC
-			// SELECT * FROM `tl_news` WHERE `pid`=2 AND `published`=1 ORDER BY tl_news.time DESC, tl_news.date DESC
 			$idArray = array();
 			$aliasArray = array();
 			$index = 0;
@@ -20,10 +18,8 @@
 				 }
 				 $index++;
 			}
-
-			// Was ist wenn es das letzte Item ist, todo
+			
 			$numItems = count($idArray)-1;
-
 
 			if($aktIndex != 0) {
 				$prevAlias= $aliasArray[$aktIndex-1];
@@ -39,12 +35,10 @@
 			if($prevAlias != "") {
 				$prevItemURL = $this->generateFrontendUrl($objPage->row(), ((isset($GLOBALS['TL_CONFIG']['useAutoItem']) && $GLOBALS['TL_CONFIG']['useAutoItem']) ?  '/' : '/items/') . $prevAlias);
 				echo "<a id='prevItem' href='$prevItemURL'>".$GLOBALS['TL_LANG']['MSC']['previous']."</a>";
-
 			}
 			if($nextAlias != "") {
 				$nextItemURL = "";
 				$nextItemURL = $this->generateFrontendUrl($objPage->row(), ((isset($GLOBALS['TL_CONFIG']['useAutoItem']) && $GLOBALS['TL_CONFIG']['useAutoItem']) ?  '/' : '/items/') . $nextAlias);
-
 				echo "<a id='nextItem' href='$nextItemURL'>".$GLOBALS['TL_LANG']['MSC']['next']."</a>";
 			}
 		}
